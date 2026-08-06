18261943d32864fe5686f7a2a95a63af31d4bd5c
Liliya2727 <syarifbintang16@gmail.com>
2026-08-06 12:48:50 +0700

    Disable trim by Default
    Signed-off-by: Liliya2727 <syarifbintang16@gmail.com>

--- mainfiles/customize.sh
+++ mainfiles/customize.sh
@@ -396,6 +396,7 @@ persist.sys.azenithconf.justintime
 persist.sys.azenithconf.disabletrace
 persist.sys.azenithconf.thermalcore
 persist.sys.azenithconf.walttunes
+persist.sys.azenithconf.fstrim
 persist.sys.azenithconf.usefpsgo
 "
 for prop in $props; do
